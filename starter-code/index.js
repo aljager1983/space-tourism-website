80db3d64c14a901417d5dd6021623e815aca9cba
unknown <al_jager1983@yahoo.com>
2023-03-18 19:24:42 +0800

    commit 4

--- starter-code/index.js
+++
@@ -1,7 +0,0 @@
-const navburger = document.getElementById("nav-burger");
-const navlist = document.getElementById("navlist");
-
-// navburger.addEventListener("click", function () {
-//     navburger.style.display = "none";
-//     navlist.style.display = "grid";
-// })
