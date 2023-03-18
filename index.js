5c38a86fc2f2b8406219236662733482440eb3b2
unknown <al_jager1983@yahoo.com>
2023-03-18 19:24:58 +0800

    commit 4

---
+++ index.js
@@ -0,0 +1,7 @@
+const navburger = document.getElementById("nav-burger");
+const navlist = document.getElementById("navlist");
+
+// navburger.addEventListener("click", function () {
+//     navburger.style.display = "none";
+//     navlist.style.display = "grid";
+// })
