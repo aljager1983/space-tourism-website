7e2a7d5fbd831d8f3668acdf8a5731f5b051540b
promdiGamer <al_jager1983@yahoo.com>
2023-03-20 22:26:06 +0800

    commit 8

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const navburger = document.getElementById("nav-burger");
 const navlist = document.getElementById("navlist");
-
+const linkHome = document.getElementById("link-home");
+const linkDestination = document.getElementById("link-destination");
+const main = document.getElementById("main")
 // navburger.addEventListener("click", function () {
 //     navburger.style.display = "none";
 //     navlist.style.display = "grid";
 // })
+
+linkDestination.addEventListener("click", function () {
+    main.style.backgroundImage = "url('assets/destination/background-destination-desktop.jpg')";
+})
+
+linkHome.addEventListener("click", function () {
+    main.style.backgroundImage = "url('assets/home/background-home-desktop.jpg')";
+})
